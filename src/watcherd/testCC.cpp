867930b434fee8ea58921c2a8710048533c5c33b
geoff lawler <geoff.lawler@sparta.com>
2008-09-18 12:39:40 -0400

    fix include stmt.

--- src/watcherd/testCC.cpp
+++ src/watcherd/testCC.cpp
@@ -1,8 +1,8 @@
 #include <boost/asio.hpp>
 
-#include "../clientConnection.hpp"
-#include "../testMessage.h"
-// #include "../gpsMessage.h"
+#include "clientConnection.hpp"
+#include "testMessage.h"
+// #include "gpsMessage.h"
 
 using namespace std;
 using namespace watcher;
@@ -17,7 +17,6 @@ int main(int argc, char **argv)
     ClientConnection cc(ioserv, "glory", "watcherd"); 
     cc.sendMessage(boost::shared_ptr<TestMessage>(new TestMessage));
     // cc.sendMessage(gpsMessagePtr(new GPSMessage(1.234, 5.678, 90.0)));
-
     ioserv.run();
 
     return 0;
