0a36422ee46dd4ba9c8852e712bb69f2e6a12cc4
geoff lawler <geoff.lawler@sparta.com>
2008-10-23 11:32:45 -0400

    watcherClientLoadAverage: print useful message when watchergraphtest is not found in the $PATH.

--- bin/watcherClientLoadAverage.py
+++ bin/watcherClientLoadAverage.py
@@ -3,6 +3,7 @@
 import time
 import re
 import subprocess
+import sys
 
 def getLoadAverage():
     # sample output ' 09:04:26 up 6 days, 22:32, 16 users,  load average: 0.08, 0.08, 0.04'
@@ -16,7 +17,12 @@ def doMain():
     while 1:
         la=getLoadAverage()
         print 'Sending load average:', la, ' to the watcher'
-        retCode=subprocess.call(['watchergraphtest', '-g', 'Load Average', '-d', str(la)])
+        try:
+            retCode=subprocess.call(['watchergraphtest', '-g', 'Load Average', '-d', str(la)])
+        except OSError:
+            print 'Caught exception when trying to run watchergraphtest, is it in your $PATH?'
+            print 'If not, type \'export $PATH=$PATH:/path/to/dir/with/watchergraphtest/in/it\' in this shell'
+            sys.exit(1)
         time.sleep(1)
 
 if __name__ == "__main__":
