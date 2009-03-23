2925455404085d47c197eab51408ab1af1d8083c
Michael Elkins <michael.elkins@sparta.com>
2009-03-23 14:23:41 -0700

    modify scheme to use time in millisecs to match message.h

--- src/watcherd/schema.sql
+++ src/watcherd/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE events (
 	id 	INTEGER PRIMARY KEY,	-- unique event identifier
-	t	REAL,			-- time at which event occurred
+	t	INTEGER,		-- time at which event occurred (milliseconds)
 	node	TEXT,			-- IP address of node
 	data	BLOB			-- binary blob containing event payload
 );
