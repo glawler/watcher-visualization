f4ccffb2c4b281074817cfd6ae2553aed0850788
geoff lawler <geoff.lawler@sparta.com>
2009-03-25 11:12:43 -0400

    Added new file: watcherTypes.h for typedefing general watcher types. May not belong here, don't know. Currently does: typedef boost::asio::ip::address NodeIdentifier.

---
+++ src/libwatcher/watcherTypes.h
@@ -0,0 +1,17 @@
+#ifndef WATCHER_TYPES_HELLO_THERE_H
+#define WATCHER_TYPES_HELLO_THERE_H
+
+#include <boost/asio.hpp>
+
+namespace watcher 
+{
+    namespace watchapi 
+    {
+        /** 
+         * nodeIdentifer is used to hide the implementation of how to uniquely identify a node. 
+         */ 
+        typedef boost::asio::ip::address NodeIdentifier;
+    }
+}
+
+#endif // WATCHER_TYPES_HELLO_THERE_H
