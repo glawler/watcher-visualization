6d02bbfdb729d3c4ab994a3e21805c27f95254b5
geoff lawler <geoff.lawler@sparta.com>
2009-03-25 10:48:43 -0400

    logger.h: use angle brackets for incluing log4cxx headers - act as if they are system headers. This helps cut down on 'make depend' as we can ignore the (hopefully unchanging) log4cxx headers.

--- include/logger.h
+++ include/logger.h
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-#include "log4cxx/logger.h"
-#include "log4cxx/basicconfigurator.h"
-#include "log4cxx/propertyconfigurator.h"
-#include "log4cxx/helpers/exception.h"
+#include <log4cxx/logger.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/propertyconfigurator.h>
+#include <log4cxx/helpers/exception.h>
 
 using namespace log4cxx;
 
