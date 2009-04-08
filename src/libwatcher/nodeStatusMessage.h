b11ec7d60534169217c3f92816d9431325d4db59
geoff lawler <geoff.lawler@sparta.com>
2009-04-08 19:04:33 -0400

    Update to nodeStatusMessage - add toStream(), operator<<(), etc. Move to new pack/unpack style of archiving. May step on ME's toes a bit.

--- src/libwatcher/nodeStatusMessage.h
+++ src/libwatcher/nodeStatusMessage.h
@@ -1,20 +1,22 @@
-/** @file
+/** 
+ * @file
  * @author Geoff Lawler <geoff.lawler@sparta.com>
  * @date 2009-03-23
  */
 #ifndef NODE_CONNECTION_MESSAGE_H
 #define NODE_CONNECTION_MESSAGE_H
 
+#include <string>
+
 #include "message.h"
 #include "watcherTypes.h"
-#include "watcherGlobalFunctions.h"
 
 namespace watcher 
 {
     namespace event 
     {
         /**
-         * Notify GUI of node connection event.
+         * Notify GUI of node status  events.
          * @author Geoff Lawler <geoff.lawler@sparta.com>
          * @date 2009-03-23
          */
@@ -24,22 +26,38 @@ namespace watcher
                 enum statusEvent 
                 {
                     connect,
-                    disconnnect
+                    disconnect
                 };
 
-                NodeStatusMessage(const statusEvent &event = connect); 
+                NodeStatusMessage(const statusEvent &event=connect, const NodeIdentifier &n=NodeIdentifier::from_string("127.0.0.1")); 
 
-                template <typename Archive> void serialize(Archive & ar, const unsigned int version);
+                NodeStatusMessage(const NodeStatusMessage &copy); 
+
+                virtual ~NodeStatusMessage();
+
+                bool operator==(const NodeStatusMessage &other) const;
+                NodeStatusMessage &operator=(const NodeStatusMessage &other);
+
+                virtual std::ostream &toStream(std::ostream &out) const;
+                std::ostream &operator<<(std::ostream &out) const; 
+
+                static std::string statusEventToString(const statusEvent &e); 
 
             private:
+
+                friend class boost::serialization::access;
+                template <typename Archive> void serialize(Archive& ar, const unsigned int version);
+
                 DECLARE_LOGGER();
 
-                statusEvent event;    // What happened
-                NodeIdentifier nodeId;
-                friend class boost::serialization::access;
-                friend std::ostream& operator<<(std::ostream&, const NodeStatusMessage&);
+                statusEvent event;      // What happened
+                NodeIdentifier nodeId;  // To whom
         };
 
+        typedef boost::shared_ptr<NodeStatusMessage> NodeStatusMessagePtr; 
+        std::ostream &operator<<(std::ostream &out, const NodeStatusMessage &mess);
     }
 }
 #endif
+
+
