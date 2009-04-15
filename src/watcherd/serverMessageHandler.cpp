d60470d4ed80f0013411e299686e4fc9c1eee8ce
geoff lawler <geoff.lawler@cobham.com>
2009-04-15 14:02:57 -0400

    server message handler always keeps the connection open (waits for new messages). Client must close connection.

--- src/watcherd/serverMessageHandler.cpp
+++ src/watcherd/serverMessageHandler.cpp
@@ -26,41 +26,11 @@ bool ServerMessageHandler::handleMessageArrive(const MessagePtr &message)
 {
     TRACE_ENTER();
 
-    bool retVal=MessageHandler::handleMessageArrive(message);
+    MessageHandler::handleMessageArrive(message);
 
-    // If it's a feeder API message, close connection, otherwise keep open
-    // to push out a message stream
-    switch(message->type)
-    {
-        // feeder API messages - close connection
-        case UNKNOWN_MESSAGE_TYPE:
-        case MESSAGE_STATUS_TYPE:
-        case TEST_MESSAGE_TYPE:
-        case GPS_MESSAGE_TYPE:
-        case LABEL_MESSAGE_TYPE:
-        case EDGE_MESSAGE_TYPE:
-        case COLOR_MESSAGE_TYPE:
-        case DATA_REQUEST_MESSAGE_TYPE:
-            retVal=false;
-            break;
-
-        // watcherdAPI messages - keep connection open
-        case SEEK_MESSAGE_TYPE:
-        case START_MESSAGE_TYPE:
-        case STOP_MESSAGE_TYPE:
-        case SPEED_MESSAGE_TYPE:
-        case NODE_STATUS_MESSAGE_TYPE:
-            retVal=true;
-            break;
-
-        // No idea.
-        case USER_DEFINED_MESSAGE_TYPE:
-            retVal=false;
-            break;
-    }
-
-    TRACE_EXIT_RET((retVal?"true":"false"));
-    return retVal;
+    // Server always keeps the connection open - client closes it when it is through with it.
+    TRACE_EXIT_RET("true");
+    return true;
 }
 
 bool ServerMessageHandler::handleMessagesArrive(const vector<MessagePtr> &messages)
