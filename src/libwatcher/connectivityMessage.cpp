84b311c447e19f3dc44cbc8cc11d72b424faa7d3
geoff lawler <geoff.lawler@cobham.com>
2009-05-06 12:57:20 -0400

    Message for sending connectity information - does not use graphic primitives, just give connection info. This'll be the default connection graph message going forward.

---
+++ src/libwatcher/connectivityMessage.cpp
@@ -0,0 +1,106 @@
+/**
+ * @file connectivityMessage.h
+ * @author Geoff Lawler <Geoff.Lawler@cobham.com>
+ * @date 2009-05-06
+ */
+
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/export.hpp>
+#include "connectivityMessage.h"
+
+using namespace std;
+
+namespace watcher 
+{
+    namespace event 
+    {
+        INIT_LOGGER(ConnectivityMessage, "Message.ConnectivityMessage");
+
+        ConnectivityMessage::ConnectivityMessage() : 
+            Message(CONNECTIVITY_MESSAGE_TYPE, CONNECTIVITY_MESSAGE_VERSION), 
+            neighbors(),
+            layer(PHYSICAL_LAYER)
+
+        {
+            TRACE_ENTER();
+            TRACE_EXIT();
+        }
+
+        // virtual
+        ConnectivityMessage::~ConnectivityMessage()  
+        {
+            TRACE_ENTER();
+            TRACE_EXIT();
+        }
+        ConnectivityMessage::ConnectivityMessage(const ConnectivityMessage &other) :
+            Message(other.type, other.version), 
+            neighbors(other.neighbors),
+            layer(PHYSICAL_LAYER)
+        {
+            TRACE_ENTER();
+            TRACE_EXIT();
+        }
+
+        bool ConnectivityMessage::operator==(const ConnectivityMessage &other) const
+        {
+            TRACE_ENTER();
+
+            bool retVal = 
+                Message::operator==(other) && 
+                neighbors == other.neighbors &&
+                layer == other.layer;
+
+            TRACE_EXIT_RET(retVal);
+            return retVal;
+        }
+
+        ConnectivityMessage &ConnectivityMessage::operator=(const ConnectivityMessage &other)
+        {
+            TRACE_ENTER();
+
+            Message::operator=(other);
+            neighbors=other.neighbors;
+            layer=other.layer;
+
+            TRACE_EXIT();
+            return *this;
+        }
+
+        // virtual 
+        std::ostream &ConnectivityMessage::toStream(std::ostream &out) const
+        {
+            TRACE_ENTER();
+
+            Message::toStream(out);
+            out << " layer: " << layer;
+            out << " neighbors:[";
+            for (vector<NodeIdentifier>::const_iterator i=neighbors.begin(); i != neighbors.end(); ++i)
+                out << *i << ",";
+            out << "] ";
+
+            TRACE_EXIT();
+            return out;
+        }
+
+        ostream &operator<<(ostream &out, const ConnectivityMessage &mess)
+        {
+            TRACE_ENTER();
+            mess.operator<<(out);
+            TRACE_EXIT();
+            return out;
+        }
+
+        template <typename Archive> void ConnectivityMessage::serialize(Archive & ar, const unsigned int /* file_version */)
+        {
+            TRACE_ENTER();
+            ar & boost::serialization::base_object<Message>(*this);
+            ar & neighbors;
+            ar & layer;
+            TRACE_EXIT();
+        }
+    }
+}
+
+BOOST_CLASS_EXPORT(watcher::event::ConnectivityMessage);
+
---
+++ src/libwatcher/connectivityMessage.h
@@ -0,0 +1,63 @@
+/**
+ * @file connectivityMessage.h
+ * @author Geoff Lawler <Geoff.Lawler@cobham.com>
+ * @date 2009-05-06
+ */
+#ifndef CONNNECTIVITY_MESSAGE_DATA_H
+#define CONNNECTIVITY_MESSAGE_DATA_H
+
+#include <vector>
+
+#include "message.h"
+#include "watcherTypes.h"
+
+namespace watcher 
+{
+    namespace event 
+    {
+        /**
+         * Inform the GUIs of the connectivity (neighbors)
+         * this node has. It's up to the GUI implementation
+         * to figure out how to display the connections.
+         * 
+         * @author Geoff Lawler <Geoff.Lawler@cobham.com>
+         * @date 2009-05-06
+         */
+        class ConnectivityMessage : public Message
+        {
+            public:
+                
+                /**
+                 * List of neighbors at the time the message is sent.
+                 */
+                std::vector<watcher::NodeIdentifier> neighbors;
+
+                /**
+                 * Which layer this is on.
+                 */
+                GUILayer layer;
+
+                ConnectivityMessage();
+                virtual ~ConnectivityMessage(); 
+
+                ConnectivityMessage(const ConnectivityMessage &other);
+
+                bool operator==(const ConnectivityMessage &other) const;
+                ConnectivityMessage &operator=(const ConnectivityMessage &other);
+
+                virtual std::ostream &toStream(std::ostream &out) const;
+                std::ostream &operator<<(std::ostream &out) const { return toStream(out); }
+
+            private:
+                friend class boost::serialization::access;
+                template <typename Archive> void serialize(Archive & ar, const unsigned int file_version);
+                DECLARE_LOGGER();
+        };
+
+        typedef boost::shared_ptr<ConnectivityMessage> ConnectivityMessagePtr; 
+
+        std::ostream &operator<<(std::ostream &out, const ConnectivityMessage &mess);
+    }
+}
+
+#endif // CONNNECTIVITY_MESSAGE_DATA_H
