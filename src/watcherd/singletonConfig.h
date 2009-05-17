1608dfe32b767742e0a7378493608df06e4e4eaa
geoff lawler <geoff.lawler@cobham.com>
2009-05-17 17:36:37 -0400

    Added comments. This class shuld be expanded to include single line load-from-file-or-use-default type functions.

--- src/watcherd/singletonConfig.h
+++ src/watcherd/singletonConfig.h
@@ -7,12 +7,24 @@
 
 namespace watcher
 {
+    /**
+     * @class SingletonConfig
+     * @author Geoff Lawler <geoff.lawler@sparta.com>
+     * @date 2009-05-15
+     *
+     * A class that wraps a libconfig Config object.
+     */
     class SingletonConfig
     {
         public:
+            
+            /** The underlaying Config object is directly accessible */
             static libconfig::Config &instance();
 
+            /** Lock the configuration when writing */
             static void lock();
+
+            /** Unlock the config when finished writing */
             static void unlock();
 
         private:
