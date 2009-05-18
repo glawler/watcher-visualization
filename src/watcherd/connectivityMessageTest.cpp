fa605475fcdd2800fb1d65c3ce58a6067e4ab08e
geoff lawler <geoff.lawler@cobham.com>
2009-05-18 11:44:19 -0400

    connectivityMessageTest: new argument: fromNode specifies which node the message "comes from".

--- src/watcherd/connectivityMessageTest.cpp
+++ src/watcherd/connectivityMessageTest.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <boost/foreach.hpp>
+#include <boost/asio/ip/address.hpp>
 
 #include <libwatcher/connectivityMessage.h>
 #include "logger.h"
@@ -13,20 +14,21 @@ using namespace watcher::event;
 
 void usage(const char *progName)
 {
-    fprintf(stderr, "Usage: %s -s|servername server [-l|logProps log.propertiesFile] nbr1 nbr2 ... nbrN\n", basename(progName)); 
-    fprintf(stderr, "Where nbrX is an ipaddress of a nore this node is connected to\n"); 
-    fprintf(stderr, "   and s|servername == the server address or hostname\n"); 
+    fprintf(stderr, "Usage: %s -s|--servername server [-l|--logProps log.propertiesFile] [-f|--fromNode fromNodeAddr] nbr1 nbr2 ... nbrN\n", basename(progName)); 
+    fprintf(stderr, "Where:\n"); 
+    fprintf(stderr, "\tnbr1 nbr2 ... nbrN is a list of ip addresses that the node is connected to.\n");
+    fprintf(stderr, "\tserver is the server address or hostname\n"); 
+    fprintf(stderr, "\tfromNodeAddr is the address of the node the neighbors are attached to.\n"); 
 
     exit(1); 
 }
 
 int main(int argc, char **argv)
 {
-    TRACE_ENTER();
-
     int c;
     char *serverName=NULL;
     char *logProps=NULL;
+    boost::asio::ip::address_v4 fromNodeAddr;
 
     while (true) 
     {
@@ -34,10 +36,11 @@ int main(int argc, char **argv)
         static struct option long_options[] = {
             {"logProps", required_argument, 0, 'l'},
             {"servername", required_argument, 0, 's'},
+            {"fromNode", required_argument, 0, 'f'},
             {0, 0, 0, 0}
         };
 
-        c = getopt_long(argc, argv, "l:s:hH?", long_options, &option_index);
+        c = getopt_long(argc, argv, "l:s:f:hH?", long_options, &option_index);
 
         if (c == -1)
             break;
@@ -49,6 +52,9 @@ int main(int argc, char **argv)
                 break;
             case 's':
                 serverName = strdup(optarg);  // buffer overflow here :) 
+                break;
+            case 'f':
+                fromNodeAddr=boost::asio::ip::address_v4::from_string(optarg);
                 break;
             case 'h':
             case 'H':
@@ -75,25 +81,29 @@ int main(int argc, char **argv)
     printf("Initializing logging system\n"); 
     LOAD_LOG_PROPS(logProps ? logProps : "log.properties");
 
+    LOG_DEBUG("Args: server: " << serverName << " fromAddr: " << fromNodeAddr << " logProps: " << logProps); 
+
     watcher::Client client(serverName); 
     printf("Connecting to %s and sending message.\n", serverName);
 
     ConnectivityMessagePtr cm(new ConnectivityMessage); 
+    if(fromNodeAddr!=boost::asio::ip::address_v4())
+    {
+        LOG_DEBUG("Setting from address on message to " << fromNodeAddr); 
+        cm->fromNodeID=fromNodeAddr;
+    }
 
-    vector<NodeIdentifier> nbrs;
     for(int i=optind; i<argc; i++)
     {
         boost::system::error_code ec;
         NodeIdentifier addr=NodeIdentifier::from_string(argv[i], ec);
         if(!ec)
-            nbrs.push_back(addr);
+            cm->neighbors.push_back(addr);
         else
             LOG_ERROR("Ignoring non address arguement: " << argv[i]);
     }
 
-    LOG_INFO("Sending this neighbor list to watcherd:"); 
-    BOOST_FOREACH(NodeIdentifier a, nbrs)
-        LOG_INFO("  " << a); 
+    LOG_DEBUG("Sending Message: " << *cm); 
 
     if(!client.sendMessage(cm))
     {
