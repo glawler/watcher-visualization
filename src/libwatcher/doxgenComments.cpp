71602f4629255b340509c98dd55c96655aec75be
geoff lawler <geoff.lawler@cobham.com>
2009-07-15 18:01:55 -0400

    a bgous "source file" which just contains comments for doxygen 'make doc'. Things in here don't seems to belong elsewhere, but should be included in the documentation.

---
+++ src/libwatcher/doxgenComments.cpp
@@ -0,0 +1,44 @@
+//
+// This file gets processed by doxygen and ends up as the main page in index.html.
+// Do not put code here, just comments for doxyen
+//
+/**
+ * @mainpage
+ *
+ * @section Introduction 
+ *
+ * The watcher system is awesome. 
+ *
+ * @section Build
+ * @subsection Dependencies
+ * 
+ * The watcher relies on a bunch of stuff.
+ *
+ * @subsection Instructions
+ *
+ * The watcher system is built using auto-tools.
+ *
+ * @section Components
+ * @subsection Test Node Daemons
+ *
+ *  @ref sendColorMessage
+ *
+ *  @ref sendConnectivityMessage
+ *
+ *  @ref sendGPSMessage 
+ *
+ * @subsection Watcher Daemon (watcherd)
+ * @subsection GUIs
+ * @subsubsection Legacy Watcher
+ * @subsubsection Watcher3d
+ *
+ * @section About
+ *
+ *  Point of contact: Geoff Lawler <geoff.lawler@cobham.com>
+ *
+ */
+
+/** 
+ * @namespace libconfig 
+ * The libconfig namespace is the namespace in the third party library libconfig++, which the watcher system uses for reading and writing cfg files 
+ */
