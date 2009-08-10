e25b5ecb81165edb951795ef4e96709fa292adae
Michael Elkins <Michael.Elkins@cobham.com>
2009-08-10 09:52:02 -0700

    add GPL short form to watcherd

--- src/watcherd/watcherd.h
+++ src/watcherd/watcherd.h
@@ -37,6 +37,24 @@ namespace watcher
 
             /** Create a new watcher daemon.
              * @param[in] ro set the writablity of the event database for the server
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
              */
             Watcherd(bool ro=false);
 
