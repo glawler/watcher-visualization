558842fc4414896078321768182469df50ce7385
Michael Elkins <Michael.Elkins@cobham.com>
2009-08-10 09:43:23 -0700

    add GPL short form to libwatcher

--- src/libwatcher/displayInfo.h
+++ src/libwatcher/displayInfo.h
@@ -56,6 +56,24 @@ namespace watcher
              * The cfg info is saved to the layer specified in the loadConfiguration() call 
              * or the default layer, if loadConfiguration is never called. 
              */
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
             virtual void saveConfiguration() = 0; 
 
             /** The layer that this info is displayed on. */
--- src/libwatcher/speedWatcherMessage.h
+++ src/libwatcher/speedWatcherMessage.h
@@ -1,3 +1,21 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /** @file
  * @author Michael Elkins <michael.elkins@sparta.com>
  * @date 2009-03-20
