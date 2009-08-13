bb02af155405141f893e19463ee201ed1ae4d885
geoff lawler <geoff.lawler@cobham.com>
2009-08-13 11:52:29 -0400

    More user guide updates.

--- src/libwatcher/doxgenComments.cpp
+++ src/libwatcher/doxgenComments.cpp
@@ -1,6 +1,24 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //
 // This file gets processed by doxygen and ends up as the main page in index.html.
-// Do not put code here, just comments for doxyen
+// Do not put code here, just comments for doxyen that don't seem to fit elsewhere.
 //
 /**
  ***************************************************************
@@ -25,24 +43,6 @@
   @ref sendColorMessage
 
   @ref sendConnectivityMessage
-
-/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
- * 
- * This file is part of WATCHER.
- * 
- *     WATCHER is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * 
- *     WATCHER is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
- * 
- *     You should have received a copy of the GNU Affero General Public License
- *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
- */
 
   @ref sendGPSMessage 
 
