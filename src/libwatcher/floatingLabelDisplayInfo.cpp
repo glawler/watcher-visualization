97f1163d740aa4ee8843b9bad1a6e7c98ac414da
geoff lawler <geoff.lawler@cobham.com>
2009-09-03 17:14:53 -0400

    new class to keep track of floating label display info.

---
+++ src/libwatcher/floatingLabelDisplayInfo.cpp
@@ -0,0 +1,63 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "floatingLabelDisplayInfo.h"
+
+using namespace std;
+using namespace watcher;
+using namespace watcher::event;
+
+INIT_LOGGER(FloatingLabelDisplayInfo, "DisplayInfo.LabelDisplayInfo.FloatingLabelDisplayInfo"); 
+
+FloatingLabelDisplayInfo::FloatingLabelDisplayInfo() : 
+    LabelDisplayInfo(),
+    lat(0.0), 
+    lng(0.0),
+    alt(0.0)
+{
+    TRACE_ENTER();
+
+    // create the "default" layer if it's not already there.
+    LabelDisplayInfo::loadConfiguration(layer); 
+
+    TRACE_EXIT();
+}
+
+// virtual
+FloatingLabelDisplayInfo::~FloatingLabelDisplayInfo()
+{
+    TRACE_ENTER();
+    TRACE_EXIT();
+}
+
+// virtual 
+ostream &FloatingLabelDisplayInfo::toStream(ostream &out) const
+{
+    TRACE_ENTER();
+    LabelDisplayInfo::toStream(out);
+    out << "lng: " << lng << " lat: " << lat << " alt: " << alt;
+    TRACE_EXIT();
+    return out; 
+}
+
+ostream& watcher::operator<<(ostream &out, const FloatingLabelDisplayInfo &obj)
+{
+    TRACE_ENTER();
+    obj.operator<<(out);
+    TRACE_EXIT();
+    return out;
+}
---
+++ src/libwatcher/floatingLabelDisplayInfo.h
@@ -0,0 +1,61 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file labelDisplayInfo.h
+ * @author Geoff Lawler <geoff.lawler@cobham.com> 
+ * @date 2009-07-15
+ */
+#ifndef FLOATING_LABEL_DISPLAY_INFO_H
+#define FLOATING_LABEL_DISPLAY_INFO_H
+
+#include "labelDisplayInfo.h"
+
+namespace watcher
+{
+    /** 
+     * Keep track of display information for a floating label (label+coordinates).
+     * @author Geoff.Lawler <Geoff.Lawler@cobham.com> 
+     */
+    class FloatingLabelDisplayInfo : public LabelDisplayInfo
+    {
+        public:
+            FloatingLabelDisplayInfo();
+            virtual ~FloatingLabelDisplayInfo(); 
+
+            /** The coordinates of the floating label */
+            float lat, lng, alt;
+
+            virtual std::ostream &toStream(std::ostream &out) const;
+            std::ostream &operator<<(std::ostream &out) const { return toStream(out); }
+
+        protected:
+
+        private:
+
+            DECLARE_LOGGER();
+    };
+
+    typedef boost::shared_ptr<FloatingLabelDisplayInfo> FloatingLabelDisplayInfoPtr; 
+
+    std::ostream &operator<<(std::ostream &out, const watcher::FloatingLabelDisplayInfo &obj);
+}
+
+
+#endif // FLOATING_LABEL_DISPLAY_INFO_H
+
