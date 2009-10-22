b17a96575da2f63b27253c457bf3d8c53dc30fe2
Michael Elkins <Michael.Elkins@cobham.com>
2009-10-21 17:04:33 -0700

    detect libkml with --enable-earthWatcher, add support for GPSMessage to earthWatcher

--- src/clients/earthWatcher/earthWatcher.cc
+++ src/clients/earthWatcher/earthWatcher.cc
@@ -27,14 +27,23 @@
 #include <getopt.h>
 #include <stdint.h>
 
+#include <cassert>
 #include <iostream>
 #include <cstdlib>
+#include <map>
 
 #include "logger.h"
 #include "initConfig.h"
 #include "singletonConfig.h"
 #include "libwatcher/messageStream.h"
 #include "libwatcher/seekWatcherMessage.h" // for SeekMessage::eof
+#include "libwatcher/gpsMessage.h"
+#include "libwatcher/edgeMessage.h"
+
+//libkml
+#include "kml/convenience/convenience.h"
+#include "kml/engine.h"
+#include "kml/dom.h"
 
 namespace {
 
@@ -67,6 +76,17 @@ void usage()
 DECLARE_GLOBAL_LOGGER(TOOL_NAME);
 
 using namespace watcher;
+
+using kmldom::ChangePtr;
+using kmldom::CreatePtr;
+using kmldom::FeaturePtr;
+using kmldom::FolderPtr;
+using kmldom::KmlPtr;
+using kmldom::PlacemarkPtr;
+using kmldom::PointPtr;
+using kmldom::UpdatePtr;
+using kmlengine::KmlFile;
+using kmlengine::KmlFilePtr;
 
 int main(int argc, char **argv)
 {
@@ -145,10 +165,88 @@ int main(int argc, char **argv)
     LOG_INFO("Starting event playback");
     ms->startStream(); 
 
+    //libkml boilerplate
+    kmldom::KmlFactory* kmlFac(kmldom::KmlFactory::GetFactory());
+    kmldom::KmlPtr kml(kmlFac->CreateKml());
+
+    //create initial DOM
+
+    FolderPtr folder(kmlFac->CreateFolder());
+    folder->set_name("Watcher");
+    kml->set_feature(folder);
+
+    //KmlFilePtr kmlFile(KmlFile::CreateFromImport(kml));
+
+    // keep a mapping from the node to its placemark object
+    typedef std::map<NodeIdentifier, PlacemarkPtr> NodeMapType;
+    typedef NodeMapType::iterator NodeMapIteratorType;
+    NodeMapType nodeMap;
+
     LOG_INFO("Waiting for events ");
     unsigned int messageNumber = 0;
-    while (ms->getNextMessage(mp))
+    while (ms->getNextMessage(mp)) {
         std::cout << "Message #" << (++messageNumber) << ": " << *mp << std::endl; 
+
+        //FeaturePtr obj;
+
+        if (mp->type == GPS_MESSAGE_TYPE) {
+            GPSMessagePtr gps( boost::dynamic_pointer_cast<GPSMessage>(mp) );
+            assert(gps->dataFormat == GPSMessage::LAT_LONG_ALT_WGS84); //only latlon is currently supported
+
+            PlacemarkPtr ptr;
+
+            NodeMapIteratorType it = nodeMap.find(mp->fromNodeID);
+            if (it == nodeMap.end()) {
+                //not found, create a new entry
+                ptr = kmlFac->CreatePlacemark();
+                ptr->set_name("node");
+                /*
+                //id and target id are both set here
+                //target id is required when changing some attribute of a feature already in the dom
+                ptr->set_id("node0");
+                ptr->set_targetid("node0");
+                */
+                nodeMap[mp->fromNodeID] = ptr;
+
+                folder->add_feature(ptr);//add to DOM
+            } else
+                ptr = it->second;
+
+            // set/update the location
+            ptr->set_geometry(kmlconvenience::CreatePointLatLon(gps->x, gps->y));
+
+            //obj = ptr;
+        } else if (mp->type == EDGE_MESSAGE_TYPE) {
+            EdgeMessagePtr edge( boost::dynamic_pointer_cast<EdgeMessage>(mp) );
+        } else
+            continue; //nothing to do
+
+        //commented out for now since it might be possible to avoid since we can stash PlacemarkPtr's in a map
+        /*
+        if (true) {
+            //adding a Feature
+            folder->add_feature(obj);
+        } else {
+            // changing a Feature
+            kmldom::ChangePtr change(kmlFac->CreateChange());
+            change->add_object(obj);
+            UpdatePtr update(kmlFac->CreateUpdate());
+            update->add_updateoperation(change);
+            kmlengine::ProcessUpdate(update, kmlFile);
+        }
+        */
+
+        if (messageNumber == 1) //temporary for testing purposes
+            break;
+    }
+
+    /*
+    std::string xml;
+    kmlFile->SerializeToString(&xml);
+    kmlbase::File::WriteStringToFile(xml, outputFile);
+    */
+    kmlbase::File::WriteStringToFile(kmldom::SerializePretty(kml), outputFile);
+
 
     // Save any configuration changes made during the run.
     LOG_INFO("Saving last known configuration to " << configFilename); 
