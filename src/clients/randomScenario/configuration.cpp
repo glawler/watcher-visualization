c395cb1ce0139cc473a1cf3b5226eb30eb0dcfa9
geoff lawler <geoff.lawler@cobham.com>
2010-03-04 10:20:04 -0500

    use 120 char line length for program options.

--- src/clients/randomScenario/configuration.cpp
+++ src/clients/randomScenario/configuration.cpp
@@ -29,10 +29,11 @@ namespace randomScenario {
         variables_map &vm=getConfig();
 
         string binName=basename(argv[0]);
+        const unsigned int lineLen=120;
 
         // Declare a group of options that will be 
         // allowed only on command line
-        options_description generic("Command line only options");
+        options_description generic("Command line only options", lineLen);
         generic.add_options()
             ("help,h", "Show help message.")
             ("configFile,c", value<string>()->default_value(binName + ".cfg"), "The location of the configuration file.");
@@ -41,7 +42,7 @@ namespace randomScenario {
         // add your configurarion paramters here
         // 
         string logLevel;
-        options_description config("Command line and configuration file options (command line will override config file)");
+        options_description config("Command line and configuration file options (command line will override config file)", lineLen);
         config.add_options()
             ("nodeNum,n", value<unsigned int>()->default_value(48), "The number of nodes in the random scenario.")
             ("nodeDegreePercentage,D", value<unsigned int>()->default_value(20), "The average number of neighbors of each node as percentagei.")
@@ -50,10 +51,10 @@ namespace randomScenario {
             ("logproperties,p", value<string>()->default_value(binName + ".log.properties"), "The log properties file")
             ("logLevel,l", value<string>(&logLevel)->default_value("debug"), "The level to log at. Valid options: trace, debug, info, warn, error, and fatal");
 
-        options_description cmdline_options;
+        options_description cmdline_options(lineLen);
         cmdline_options.add(generic).add(config); 
 
-        options_description config_file_options;
+        options_description config_file_options(lineLen);
         config_file_options.add(config); 
 
         store(parse_command_line(argc, argv, cmdline_options), vm);
