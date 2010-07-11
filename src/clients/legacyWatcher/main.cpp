60b4f234f69d9d5a3bc9ce7c22cd9455467187d9
geoff lawler <geoff.lawler@cobham.com>
2010-07-11 11:58:04 -0400

    rewrote argument and initialization code in main for legWatcher. Do not use broken initConfig. Support --maxNodes, --logLevel, --maxLayers, --logPropsFile, and --configFile on the command line and for maxNodes, maxLayers, and logPropsFile, the command line overrides anything found in the cfg. file. Create files properly if they don't exist.

--- src/clients/legacyWatcher/main.cpp
+++ src/clients/legacyWatcher/main.cpp
@@ -22,17 +22,18 @@
  * @date 2009-07-15 
  */
 #include <iostream>
+#include <fstream>
 #include <QTimer>
 #include <string>
 #include <GL/glut.h>
 #include <getopt.h>
+#include <boost/filesystem.hpp>
 
 #include "watcherMainWindow.h"
 #include "watcherScrollingGraphControl.h"
 
 #include "logger.h"
 #include "libconfig.h++"
-#include "initConfig.h"
 #include "singletonConfig.h"
 
 using namespace std;
@@ -45,61 +46,137 @@ int main(int argc, char *argv[])
 {
     TRACE_ENTER();
 
-    const char *usageString=" -c,--configFile=cfgFile [-s,--server=server][-l,--logLevel=[debug|info|warn|error|fatal|off] -h,--help"; 
+    const char *usageString=" [options]\n" 
+        "\t-c,--configFile=file\t\tUse this file to read watcher configuration information from.\n"
+        "\t-s,--server=addr/name\t\tAddress or hostname of the watcher daemon to connect to.\n"
+        "\t-n,--maxNodes=integer\t\tMaximum number of nodes in this test run.\n"
+        "\t-y,--maxLayers=integer\t\tMaximum number of layers in this test run.\n"
+        "\t-l,--logLevel=level\t\tSet the default log level. Must be one of\n"
+        "                     \t\t\toff, fatal, error, warn, info, debug, or trace.\n"
+        "\t-L,--logProperties=filename\t\tThe log.properties filename\n"
+        ;
 
-    for (int i=0; i!=argc; i++)
-        if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help") || !strcmp(argv[i], "-?")) {
-            cout << "Usage : " << basename(argv[0]) << usageString << endl;
-            exit(EXIT_FAILURE);
-        }
-
-    string configFilename;
-    SingletonConfig::lock(); 
     Config &config=SingletonConfig::instance(); 
-    try {
-        if (false==initConfig(config, argc, argv, configFilename))
-        {
-            cerr << "Error reading configuration file, unable to continue." << endl;
-            cerr << "Usage: " << basename(argv[0]) << usageString << endl;
-            return 1;
+    SingletonConfig::lock();
+
+    string server, logLevel;
+    string configFilename(string(basename(argv[0]))+string(".cfg")); 
+    string logPropsFilename(string(basename(argv[0]))+string(".log.properties")); 
+    int maxLayers=-1, maxNodes=-1;
+    while (true) {
+        int option_index = 0;
+        opterr=1;       // don't allow unknown args. 
+
+        static struct option long_options[] = {
+            {"server", required_argument, 0, 's'},
+            {"maxNodes", required_argument, 0, 'n'}, 
+            {"maxLayers", required_argument, 0, 'y'},
+            {"logProperties", required_argument, 0, 'L'},
+            {"logLevel", required_argument, 0, 'l'},
+            {"configFile", required_argument, 0, 'c'},
+            {"help", no_argument, 0, 'h'},
+            {0,0,0,0}
+        };
+
+        int c = getopt_long(argc, argv, "c:l:L:n:s:y:h", long_options, &option_index);
+
+        if (c==-1) 
+            break;
+
+        switch (c) { 
+            case 'c':
+                configFilename=optarg;
+                break;
+            case 's':
+                server=optarg;
+                break;
+            case 'n':
+                maxNodes=boost::lexical_cast<int>(optarg); 
+                break;
+            case 'l':
+                logLevel=optarg;
+                if (logLevel!="off" && logLevel!="fatal" && logLevel!="error" && logLevel!="warn" && 
+                        logLevel!="info" && logLevel!="debug" && logLevel!="trace") { 
+                    cout << endl << "logLevel must be one of off, fatal, error, warn, info, debug, or trace." << endl;
+                    cout << "Usage : " << endl << basename(argv[0]) << usageString << endl;
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'L':
+                logPropsFilename=optarg;
+                break;
+            case 'y':
+                maxLayers=boost::lexical_cast<int>(optarg); 
+                break;
+            case 'h':
+            case '?':
+                cout << "Usage : " << endl << basename(argv[0]) << usageString << endl;
+                exit(EXIT_FAILURE); 
+                break;
         }
+
+    }
+    try {
+        // make sure it exists. 
+        if (!boost::filesystem::exists(configFilename)) {
+            cerr << "Configuration file \"" << configFilename << "\", not found. Creating it." << endl;
+            ofstream f(configFilename.c_str(), ios_base::in | ios_base::out); 
+            f.close();
+        }
+        else 
+            config.readFile(configFilename.c_str());
         SingletonConfig::setConfigFile(configFilename);
     }
-    catch (const SettingException &e) {
-        LOG_ERROR("Error in configuration setting \"" << e.getPath() << "\"");
+    catch (ParseException &e)
+    {
+        cerr << "Error reading configuration file " << configFilename << ": " << e.what() << endl;
+        cerr << "Error: \"" << e.getError() << "\" on line: " << e.getLine() << endl;
+        exit(EXIT_FAILURE);  // !!!
     }
 
-    // Override config for server if given on the command line
-    for (int i=0; i!=argc; i++) {
-        if (!strcmp(argv[i], "-s") || !strcmp(argv[i], "--server")) { 
-            if (i!=argc) { 
-                libconfig::Setting &root=config.getRoot();
-                string prop="server";
-                if (!root.exists(prop))
-                    root.add(prop, Setting::TypeString);
-                root[prop]=argv[i+1]; 
-                i++;
-            }
-            else { 
-                cerr << "Error in \"server\" arguement: you must specify the server!" << endl;
-                exit(EXIT_FAILURE); 
-            }
-        }
+    // now that the default config has been loaded (or not), load the command line args
+    // to override.
+    libconfig::Setting &root=config.getRoot();
+    if (!root.lookupValue("logProperties", logPropsFilename)) {
+        cerr << "Unable to find logProperties setting in the configuration file, using default: " << logPropsFilename << endl;
+        root.add("logProperties", Setting::TypeString)=logPropsFilename;
     }
+    else 
+        root["logProperties"]=logPropsFilename;
 
+    if (!server.empty()) {
+        if (!root.lookupValue("server", server))
+            root.add("server", Setting::TypeString)=server;
+        else
+            root["server"]=server;
+    }
+    if (-1!=maxNodes) {
+        if (!root.lookupValue("maxNodes", maxNodes))
+            root.add("maxNodes", Setting::TypeInt)=maxNodes;
+        else
+            root["maxNodes"]=maxNodes;
+    }
+    if (-1!=maxLayers) {
+        if (!root.lookupValue("maxLayers", maxLayers))
+            root.add("maxLayers", Setting::TypeInt)=maxLayers;
+        else
+            root["maxLayers"]=maxLayers;
+    }
     SingletonConfig::unlock();
 
-    string logConf("watcher.log.properties");
-    if (!config.lookupValue("logProperties", logConf))
-    {
-        cerr << "Unable to find logproperties setting in the configuration file, using default: " << logConf << endl;
-        config.getRoot().add("logProperties", Setting::TypeString)=logConf;
+
+    if (!boost::filesystem::exists(logPropsFilename)) {
+        cerr << "Log properties file not found - logging disabled." << endl;
+        Logger::getRootLogger()->setLevel(Level::getOff());
     }
-
-    LOAD_LOG_PROPS(logConf); 
-
-    LOG_INFO("Logger initialized from file \"" << logConf << "\"");
-    LOG_INFO("Although the legacy watcher code does not use it, so it is not overly valuable");
+    else {
+        LOAD_LOG_PROPS(logPropsFilename); 
+        LOG_INFO("Logger initialized from file \"" << logPropsFilename << "\"");
+    }
+    if (!logLevel.empty()) {
+        cout << "Setting default log level to " << logLevel << endl;
+        Logger::getRootLogger()->setLevel(Level::toLevel(logLevel)); 
+    }
 
     QApplication::setColorSpec(QApplication::CustomColor);
     QApplication app(argc, argv);
@@ -148,6 +225,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE; 
     }
 
+    // ...and go.
     window->show();
 
     TRACE_EXIT();
