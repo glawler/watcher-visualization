b4eb09fa46734a8d84e8b414348bc886d0ec05a4
geoff lawler <geoff.lawler@cobham.com>
2010-07-15 18:03:39 -0400

    these new files implement the layer configuration dialog.

---
+++ src/clients/legacyWatcher/layerConfigurationDialog.cpp
@@ -0,0 +1,212 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** 
+ * @file layerConfigurationDialog.cpp
+ * @author Geoff Lawler <geoff.lawler@cobham.com>
+ * @date 2010-07-13
+ */
+#include <Qt/qcolordialog.h>
+#include <Qt/qfontdialog.h>
+#include <Qt/qmessagebox.h>
+#include <boost/foreach.hpp>
+#include "layerConfigurationDialog.h"
+#include "logger.h"
+
+using namespace std;
+
+namespace watcher { 
+
+    INIT_LOGGER(LayerConfigurationDialog, "LayerConfigurationDialog"); 
+
+    LayerConfigurationDialog::LayerConfigurationDialog(QWidget *parent, Qt::WindowFlags f) : 
+        QDialog(parent, f), currentLayer(NULL)
+    {
+        TRACE_ENTER();
+        setupUi(this);
+
+        // previewWindow.setLayerData(&l); 
+
+        TRACE_EXIT();
+    }
+
+    LayerConfigurationDialog::~LayerConfigurationDialog()
+    {
+        TRACE_ENTER();
+        // remove test label and test floating label
+        TRACE_EXIT();
+    }
+
+    void LayerConfigurationDialog::addLayer(WatcherLayerData *l)
+    {
+        layers.push_back(l);
+        QListWidgetItem *item=new QListWidgetItem(l->layerName.c_str(), layerListWidget); 
+        if (l->isActive) {
+            QFont f(item->font());
+            f.setBold(true); 
+            item->setFont(f); 
+        }
+    }
+
+    void LayerConfigurationDialog::layerToggle(std::string &name)
+    {
+        QString layerName(name.c_str()); 
+        QList<QListWidgetItem *> items=layerListWidget->findItems(layerName, Qt::MatchExactly); 
+        BOOST_FOREACH(QListWidgetItem *item, items) { 
+            QFont f(item->font());
+            f.setBold(!f.bold()); 
+            item->setFont(f); 
+        }
+    }
+
+    bool LayerConfigurationDialog::checkForValidCurrentLayer()
+    {
+        if (!currentLayer) {
+            QMessageBox::information(this, tr("Layer Selection"), tr("Please select a layer first.")); 
+            return false;
+        }
+        return true;
+    }
+
+    void LayerConfigurationDialog::setFont(string &fontFamily, float &pointSize)
+    {
+        QFont initial(fontFamily.c_str(), pointSize); 
+        bool ok;
+        QFont font=QFontDialog::getFont(&ok, initial, this); 
+        if (ok) {
+            fontFamily=font.family().toStdString(); 
+            pointSize=font.pointSize(); 
+        }
+    }
+
+    void LayerConfigurationDialog::setEdgeLabelFont()
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setFont(currentLayer->edgeDisplayInfo.labelFont, currentLayer->edgeDisplayInfo.labelPointSize); 
+    }
+
+    void LayerConfigurationDialog::setNodeLabelFont()
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setFont(currentLayer->referenceLabelDisplayInfo.fontName, currentLayer->referenceLabelDisplayInfo.pointSize); 
+    }
+
+    void LayerConfigurationDialog::setFloatingLabelFont()
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setFont(currentLayer->referenceFloatingLabelDisplayInfo.fontName, currentLayer->referenceFloatingLabelDisplayInfo.pointSize); 
+    }
+
+    void LayerConfigurationDialog::setCurrentLayer(QListWidgetItem *item)
+    {
+        string itemText(item->text().toStdString()); 
+        BOOST_FOREACH(WatcherLayerData *l, layers) { 
+            if (l && l->layerName==itemText) {
+                currentLayer=l;
+                configureDialogWithCurrentLayer(); 
+                break;
+            }
+        }
+    }
+
+    void LayerConfigurationDialog::configureDialogWithCurrentLayer()
+    {
+        // set colored buttons
+        struct {
+            QToolButton *b;
+            const Color &c;
+        } cbs[] = {
+            { edgeColorButton, currentLayer->edgeDisplayInfo.color }, 
+            { edgeLabelColorButton, currentLayer->edgeDisplayInfo.labelColor },
+            { floatingLabelColorButton, currentLayer->referenceFloatingLabelDisplayInfo.foregroundColor }, 
+            { nodeLabelColorButton, currentLayer->referenceLabelDisplayInfo.foregroundColor }, 
+        };
+        for (size_t i=0; i<(sizeof(cbs)/sizeof(cbs[0])); i++) {
+            QString ss;
+            ss.sprintf("background-color: #%02x%02x%02x", cbs[i].c.r, cbs[i].c.g, cbs[i].c.b); 
+            cbs[i].b->setStyleSheet(ss); 
+        }
+        // set edit boxes
+        edgeLabelEditBox->setText(currentLayer->edgeDisplayInfo.label.c_str()); 
+    }
+
+    void LayerConfigurationDialog::setColor(Color &c, QToolButton *b)
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        if (currentLayer) { 
+            QRgb rgb=0x00000000;
+            rgb |= (0x00ff0000 & c.r); 
+            rgb |= (0x0000ff00 & c.g); 
+            rgb |= (0x000000ff & c.b); 
+            rgb |= (0xff000000 & c.a); 
+
+            bool ok=false;
+            rgb=QColorDialog::getRgba(rgb, &ok);
+            if (ok) {
+                c=Color(qRed(rgb), qGreen(rgb), qBlue(rgb), qAlpha(rgb)); 
+                QString ss;
+                ss.sprintf("background-color: #%02x%02x%02x", qRed(rgb), qGreen(rgb), qBlue(rgb)); 
+                b->setStyleSheet(ss); 
+            }
+        }
+    }
+
+    void LayerConfigurationDialog::setNodeLabelColor() 
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setColor(currentLayer->referenceLabelDisplayInfo.foregroundColor, nodeLabelColorButton); 
+    }
+    void LayerConfigurationDialog::setFloatingLabelColor() 
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setColor(currentLayer->referenceFloatingLabelDisplayInfo.foregroundColor, floatingLabelColorButton); 
+    }
+    void LayerConfigurationDialog::setEdgeLabelColor() 
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setColor(currentLayer->edgeDisplayInfo.labelColor, edgeLabelColorButton); 
+    }
+    void LayerConfigurationDialog::setEdgeColor() 
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        setColor(currentLayer->edgeDisplayInfo.color, edgeColorButton); 
+    }
+    void LayerConfigurationDialog::setEdgeWidth(int w)
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        currentLayer->edgeDisplayInfo.width=w;
+    }
+    void LayerConfigurationDialog::updateEdgeLabel(QString text)
+    {
+        if (!checkForValidCurrentLayer()) 
+            return;
+        string edgeLabel=text.toStdString(); 
+        LOG_INFO("Layerconfig: Set edge label to " << edgeLabel << " on layer " << currentLayer->layerName); 
+        currentLayer->edgeDisplayInfo.label=text.toStdString();
+    }
+}
+
---
+++ src/clients/legacyWatcher/layerConfigurationDialog.h
@@ -0,0 +1,95 @@
+
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** 
+ * @file layerConfiguyrationDialog.h.h
+ * @author Geoff Lawler <geoff.lawler@cobham.com>
+ * @date 2010-07-13
+ */
+#ifndef LAYER_WATCHER_CONFIG_INFO_DIALOG_H
+#define LAYER_WATCHER_CONFIG_INFO_DIALOG_H
+
+#include <QtGui/QDialog>
+#include <string>
+#include "ui_layerConfigurationDialog.h"
+#include "libwatcher/watcherLayerData.h"
+#include "layerPreviewWindow.h"
+#include "declareLogger.h"
+
+namespace watcher
+{
+    class LayerConfigurationDialog : public QDialog, public Ui::layerConfigurationDialog
+    {
+        Q_OBJECT
+
+        public:
+
+            explicit LayerConfigurationDialog(QWidget *parent = 0, Qt::WindowFlags f = 0);
+            ~LayerConfigurationDialog(); 
+
+        public slots:
+
+            // layer stuff
+            void addLayer(WatcherLayerData *l); 
+            void setCurrentLayer(QListWidgetItem*);
+            void layerToggle(std::string &name); 
+
+            // edges
+            void updateEdgeLabel(QString text);
+            void setEdgeColor();
+            void setEdgeLabelColor();
+            void setEdgeLabelFont();
+            void setEdgeWidth(int w);
+
+            // labels (attached)
+            void setNodeLabelColor();
+            void setNodeLabelFont(); 
+
+            // labels (floating)
+            void setFloatingLabelColor();
+            void setFloatingLabelFont();
+
+        signals:
+
+        protected:
+    
+            bool checkForValidCurrentLayer();
+            void setColor(Color &c, QToolButton *b);
+            void setFont(std::string &fontFamily, float &pointSize);
+
+            /** 
+             * Set the dialog widgets to reflect the currently selected layer. 
+             * Assumes currentLayer is set.
+             */
+            void configureDialogWithCurrentLayer(); 
+            
+            typedef std::list<WatcherLayerData *> LayerList;
+            LayerList layers; 
+
+            WatcherLayerData *currentLayer;
+
+            LayerPreviewWindow previewWindow;
+
+        private:
+
+            DECLARE_LOGGER();
+    };
+}
+
+#endif // LAYER_WATCHER_CONFIG_INFO_DIALOG_H
