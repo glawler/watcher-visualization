06f6c4da34019ac6ce3a2140c9da3416b0382155
geoff lawler <geoff.lawler@cobham.com>
2010-11-09 11:49:50 -0500

    added [-s server] and [-l loglevel] to command line args for dataWatcher.

--- src/clients/dataWatcher/watcherConfig.cpp
+++ src/clients/dataWatcher/watcherConfig.cpp
@@ -37,7 +37,9 @@ class singleton_config_lock {
 
 void usage(const char *prog)
 {
-    std::cout << "usage: " << prog << " [ -c CONFIG ] [ -h ] [ -n STREAMUID ]" << std::endl;
+    std::cout << "usage: " << prog << " [ -c CONFIG ] [ -h ] [ -n STREAMUID ] [ -s WATCHERSERVER ] [ -l LOGLEVEL ]" << std::endl;
+	std::cout << "	WATCHERSERVER - the address or resolvable name of the watcher server." << std::endl;
+	std::cout << "	LOGLEVEL - one of off, fatal, error, warn, info, debug, or trace. " << std::endl;
     exit(0);
 }
 
@@ -46,63 +48,83 @@ void usage(const char *prog)
 DECLARE_GLOBAL_LOGGER("watcherGlobalLogger");
 
 namespace watcher {
-namespace config {
-    std::string Server("localhost");
-    int StreamUid = -1;
+	namespace config {
+		std::string Server;
+		int StreamUid = -1;
 
-void initialize(int argc, char **argv)
-{
-    libconfig::Config& config = watcher::SingletonConfig::instance();
-    singleton_config_lock lock;
+		void initialize(int argc, char **argv)
+		{
+			libconfig::Config& config = watcher::SingletonConfig::instance();
+			singleton_config_lock lock;
+			bool serverSpecified=false;
 
-    std::string configFilename(std::string(boost::filesystem::basename(argv[0])) + ".cfg");
-    std::string logPropsFilename(std::string(boost::filesystem::basename(argv[0])) + ".log.properties");
+			std::string configFilename(std::string(boost::filesystem::basename(argv[0])) + ".cfg");
+			std::string logPropsFilename(std::string(boost::filesystem::basename(argv[0])) + ".log.properties");
+			std::string logLevel;
 
-    int i;
-    while ((i = getopt(argc, argv, "c:n:h")) != -1) {
-	switch(i) {
-	    case 'c':
-		configFilename = std::string(optarg);
-		break;
-	    case 'n':
-		StreamUid = strtol(optarg, 0, 0);
-		break;
-	    case 'h':
-		usage(argv[0]);
-	}
-    }
+			int i;
+			while ((i = getopt(argc, argv, "l:s:c:n:h")) != -1) {
+				switch(i) {
+					case 'c':
+						configFilename = std::string(optarg);
+						break;
+					case 'n':
+						StreamUid = strtol(optarg, 0, 0);
+						break;
+					case 's':
+						Server = std::string(optarg);
+						serverSpecified=true;
+						break;
+					case 'l':
+						logLevel = std::string(optarg);
+						if (logLevel!="off" && logLevel!="fatal" && logLevel!="error" && logLevel!="warn" && 
+								logLevel!="info" && logLevel!="debug" && logLevel!="trace") { 
+							std::cerr << std::endl << "logLevel must be one of off, fatal, error, warn, info, debug, or trace." << std::endl;
+							exit(EXIT_FAILURE);
+						}
 
-    try {
-        // make sure it exists. 
-        if (!boost::filesystem::exists(configFilename)) {
-	    std::cerr << "Configuration file \"" << configFilename << "\", not found. Creating it." << std::endl;
-	    std::ofstream f(configFilename.c_str(), std::ios_base::in | std::ios_base::out); 
-            f.close();
-        } else 
-            config.readFile(configFilename.c_str());
-	watcher::SingletonConfig::setConfigFile(configFilename);
-    } catch (libconfig::ParseException &e) {
-	std::cerr << "Error reading configuration file " << configFilename << ": " << e.what() << std::endl;
-	std::cerr << "Error: \"" << e.getError() << "\" on line: " << e.getLine() << std::endl;
-        exit(EXIT_FAILURE);  // !!!
-    }
+						break;
+					case 'h':
+						usage(argv[0]);
+				}
+			}
 
-    libconfig::Setting& root = config.getRoot();
-    if (!root.lookupValue("logProperties", logPropsFilename))
-	root.add("logProperties", libconfig::Setting::TypeString) = logPropsFilename;
-    if (!root.lookupValue("server", watcher::config::Server))
-	root.add("server", libconfig::Setting::TypeString) = watcher::config::Server;
+			try {
+				// make sure it exists. 
+				if (!boost::filesystem::exists(configFilename)) {
+					std::cerr << "Configuration file \"" << configFilename << "\", not found. Creating it." << std::endl;
+					std::ofstream f(configFilename.c_str(), std::ios_base::in | std::ios_base::out); 
+					f.close();
+				} else 
+					config.readFile(configFilename.c_str());
+				watcher::SingletonConfig::setConfigFile(configFilename);
+			} catch (libconfig::ParseException &e) {
+				std::cerr << "Error reading configuration file " << configFilename << ": " << e.what() << std::endl;
+				std::cerr << "Error: \"" << e.getError() << "\" on line: " << e.getLine() << std::endl;
+				exit(EXIT_FAILURE);  // !!!
+			}
 
-    if (!boost::filesystem::exists(logPropsFilename)) {
-	std::cerr << "Log properties file not found - logging disabled." << std::endl;
-	Logger::getRootLogger()->setLevel(Level::getOff());
-    } else {
-	LOAD_LOG_PROPS(logPropsFilename); 
-	LOG_INFO("Logger initialized from file \"" << logPropsFilename << "\"");
-    }
-}
-
-} // config
+			libconfig::Setting& root = config.getRoot();
+			if (!root.lookupValue("logProperties", logPropsFilename))
+				root.add("logProperties", libconfig::Setting::TypeString) = logPropsFilename;
+			if (Server.empty()) { 
+				Server="localhost";
+				if (!root.lookupValue("server", watcher::config::Server))
+					root.add("server", libconfig::Setting::TypeString) = watcher::config::Server;
+			}
+			if (!boost::filesystem::exists(logPropsFilename)) {
+				std::cerr << "Log properties file not found - logging disabled." << std::endl;
+				Logger::getRootLogger()->setLevel(Level::getOff());
+			} else {
+				LOAD_LOG_PROPS(logPropsFilename); 
+				LOG_INFO("Logger initialized from file \"" << logPropsFilename << "\"");
+			}
+			if (!logLevel.empty()) {
+				std::cout << "Setting default log level to " << logLevel << std::endl;
+				Logger::getRootLogger()->setLevel(Level::toLevel(logLevel));
+			}
+		}
+	} // config
 } // watcher
 
 // vim:sw=4
